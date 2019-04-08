be1a65b9cd3d26429041a32a6eb784e5b7205e8f
zhaojian <617762941@qq.com>
2019-04-08 23:16:11 +0800

    淡定

---
+++ src/main/java/com/cike01/cike01/App.java
@@ -0,0 +1,19 @@
+package com.cike01.cike01;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+    	try {
+			Class.forName("com.mysql.jdbc.Driver");
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}System.out.println("OK");
+        
+    }
+}
